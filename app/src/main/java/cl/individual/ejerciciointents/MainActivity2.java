0e0ac7d98aade38978984b75d50f0ee280e9837c
Emilia <correoalcantaraemilia@gmail.com>
2023-06-22 21:45:03 -0400

    Diseño básico inicial. Se da funciones a botones para ir a la segunda pantalla, para ir al url, y para volver de la segunda pantalla a la primera. Pendiente: el botón salir en la primera pantalla.

---
+++ app/src/main/java/cl/individual/ejerciciointents/MainActivity2.java
@@ -0,0 +1,41 @@
+package cl.individual.ejerciciointents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        initListener();
+    }
+
+    private void initListener() {
+        Button botonUrl = findViewById(R.id.botonPaginaWeb);
+        Button botonVolver = findViewById(R.id.botonAtras);
+
+        botonUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent irPagWeb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://kotlindoc.blogspot.com/2019/10/android-log-con-timber.html"));
+                startActivity(irPagWeb);
+            }
+        });
+
+        botonVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent volverAtras = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(volverAtras);
+            }
+        });
+    }
+}
