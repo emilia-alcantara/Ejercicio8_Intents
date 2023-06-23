80b38b8fd0d6b13ee7fe6e2adbaf51512cb1f856
Emilia <correoalcantaraemilia@gmail.com>
2023-06-23 01:36:09 -0400

    update diseño + log en cada método del ciclo de vida.

--- app/src/main/java/cl/individual/ejerciciointents/MainActivity2.java
+++ app/src/main/java/cl/individual/ejerciciointents/MainActivity2.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +39,41 @@ public class MainActivity2 extends AppCompatActivity {
                 onBackPressed();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "----onStart----");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "----onResume----");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "----onPause----");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "----onStop----");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "----onDestroy----");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "----onRestart----");
     }
 }
